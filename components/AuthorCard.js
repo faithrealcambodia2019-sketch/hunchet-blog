3a85f99e88c36951d2e132ca6ec3955114a0941e
faithrealcambodia2019-sketch <faithrealcambodia2019@gmail.com>
2026-08-04 13:53:14 +0700

    Create AuthorCard component for author bio display
    This component displays an author's bio, including their image, name, description, and social links.

---
+++ components/AuthorCard.js
@@ -0,0 +1,42 @@
+export default function AuthorCard() {
+  return (
+    <div className="author-bio">
+      <div className="author-avatar">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://hunchetblog.wordpress.com/wp-content/uploads/2026/06/d3270-untitled-design-1-1.png"
+          alt="Hun Chet"
+        />
+      </div>
+      <div className="author-text">
+        <strong>Hun Chet</strong>
+        <span>
+          Sharing the Gospel through biblical encouragement, Christian
+          teaching, prayer, and ministry content that points hearts to Jesus
+          Christ.
+        </span>
+        <div className="author-social-links">
+          <a href="tel:0966875886" className="author-btn btn-phone">
+            096 687 5886
+          </a>
+          <a
+            href="https://t.me/+855966875886"
+            target="_blank"
+            rel="noreferrer"
+            className="author-btn btn-telegram"
+          >
+            Telegram
+          </a>
+          <a
+            href="https://www.facebook.com/hunchet2024/"
+            target="_blank"
+            rel="noreferrer"
+            className="author-btn btn-facebook"
+          >
+            Facebook
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
