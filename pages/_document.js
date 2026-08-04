16a2fd4913dc8de27a51da0e8dc4af46fd994b93
faithrealcambodia2019-sketch <faithrealcambodia2019@gmail.com>
2026-08-04 16:48:51 +0700

    Update favicon and apple-touch-icon links

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { FAVICON, APPLE_ICON } from "../lib/media";
 
 export default function Document() {
   return (
     <Html lang="km">
       <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href={FAVICON} type="image/png" sizes="any" />
+        <link rel="shortcut icon" href={FAVICON} type="image/png" />
+        <link rel="apple-touch-icon" href={APPLE_ICON} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;800;900&family=Inter:wght@400;500;600;700&family=Noto+Sans+Khmer:wght@400;600;700&family=Noto+Serif+Khmer:wght@600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;800;900&family=Inter:wght@400;500;600;700&family=Koh+Santepheap:wght@400;700;900&display=swap"
           rel="stylesheet"
         />
         <meta name="theme-color" content="#0f2a5c" />
